7ab161f06876f14e947cf5bc35afee128c5d91b4
Safal Mukhia <safal.mukhia@ibm.com>
2024-02-23 16:05:42 +0000

    vector db connector update
    Signed-off-by: Safal Mukhia <safal.mukhia@ibm.com>

--- connectors/vector_db_connector.py
+++ connectors/vector_db_connector.py
@@ -1,8 +1,8 @@
 import os
 from dotenv import load_dotenv
+import sys
 
-import time
-import random
+from utils.files_handler import FileHandler
 from pymilvus import connections, utility
 from pymilvus import Collection, DataType, FieldSchema, CollectionSchema
 
@@ -13,31 +13,39 @@ class MilvusConnector:
         """
         Zilliz Cloud (Milvus) vector db connector class.
         """
+        # AUTHENTICATION
         # store connection keys from .env file by loading it as environment variables.
         load_dotenv()
-        # we will use the serverless connection method
-        # which only requires the uri and token (API key).
+        # we will use the serverless connection method which only requires the uri and token (API key).
         self.zilliz_uri = os.environ['ZILLIZ_URI']
         self.token = os.environ['ZILLIZ_API_KEY']
+
         # store the created schema here.
-        self.schema = {}
+        self.schema = None
         self.server = "default"
-        self.collection_name = 'test'
+        self.collection_name = 'my_collection'
+        self.collection = None
+
+        # get params from config for building and searching indexes.
+        file_handler = FileHandler()
+        file_handler.get_config(config_file_name='vector_db_config')
+        self.index_building_params = file_handler.config['MILVUS']['INDEX_BUILDING_PARAMS']
+        self.search_params = file_handler.config['MILVUS']['SEARCH_PARAMS']
+
+        # instantiating the class will connect to the server instance of Zilliz cloud cluster specified
+        # in the .env file and stored in self.zilliz_uri and self.token attributes.
+        connections.connect(self.server,
+                            uri=self.zilliz_uri,
+                            token=self.token)
+        print(f"Connected to DB: {self.zilliz_uri}")
 
     def set_collection_name(self, collection_name):
         """A function to assign the collection name. Otherwise, it will be called
         default_collection"""
         self.collection_name = collection_name
 
-    def __connect_to_db(self):
-        connections.connect(self.server,
-                            uri=self.zilliz_uri,
-                            token=self.token)
-        print(f"Connecting to DB: {self.zilliz_uri}")
-
     def drop_collect(self):
         # first connect to DB.
-        self.__connect_to_db()
         # check to see if collection exists.
         check_collection = utility.has_collection(self.collection_name)
         if check_collection:
@@ -57,23 +65,22 @@ class MilvusConnector:
         :param field_name: type.FLOAT_VECTOR to store the embeddings.
         :param embeddings_dim: the dimension of the embedding for the field.
         """
-        collection_name = "collection"
-        if len(self.collection_name) > 0:
-            collection_name = self.collection_name
-
-        p_id = FieldSchema(
-            name=primary_id,
-            dtype=DataType.INT64,
-            is_primary=True
-        )
-        vector_field = FieldSchema(
-            name=field_name,
-            dtype=DataType.FLOAT_VECTOR,
-            dim=embeddings_dim
-        )
+        fields = [
+            FieldSchema(
+                name=primary_id,
+                dtype=DataType.INT64,
+                is_primary=True,
+                auto_id=True
+            ),
+            FieldSchema(
+                name=field_name,
+                dtype=DataType.FLOAT_VECTOR,
+                dim=embeddings_dim
+            )
+        ]
         self.schema = CollectionSchema(
-            fields=[p_id, vector_field],
-            description=f"A Schema for the {collection_name} collection",
+            fields=fields,
+            description=f"A Schema for the {self.collection_name} collection",
             enable_dynamic_field=True
         )
 
@@ -82,11 +89,53 @@ class MilvusConnector:
         A function to create a collection.
         :param shards_num: the number of shards to use.
         :param server: the server to create the collection on.
+        :param schema: the schema to be used to create the collection.
         """
+        if not str(type(self.schema)) == "<class 'pymilvus.orm.schema.CollectionSchema'>":
+            print("""Please create a schema before creating a collection.
+            This can be done using the create_default_schema method or by assigning your custom
+            schema by assigning it to the .schema attribute of your MilvusConnector instance.""")
+            raise TypeError
+
         collection = Collection(
             name=self.collection_name,
             schema=self.schema,
             using=server,
             shards_num=shards_num
         )
-        return collection
+        self.collection = collection
+
+    def create_index(self, field_name):
+        """
+        A function to create index using attributes stored in the class.
+        """
+        if isinstance(self.collection, type(None)):
+            self.create_collection()
+
+        self.collection.create_index(
+            field_name=field_name,
+            index_params=self.index_building_params
+        )
+
+    def search_collection(self, field_name, query_vector, limit=10, expr=None):
+        """
+        A function to search collection using query vectors.
+        :param field_name: name of the vector field to search on.
+        :param query_vector: the query vector to search with.
+        :param limit: the limit to the number of embeddings retrieved.
+        :param expr: Bool, used to filter attributes. See: https://milvus.io/docs/boolean.md
+        """
+        if isinstance(self.collection, type(None)):
+            print("""Please create the collection first using the .create_collection method""")
+            sys.exit()
+            
+        else:
+            self.collection.load()
+            results = self.collection.search(
+                data=query_vector,
+                anns_field=field_name,
+                param=self.search_params,
+                limit=limit,
+                expr=expr
+            )
+            return results
